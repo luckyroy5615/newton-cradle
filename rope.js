000d5c7da27df847248d765609433da330e4bd26
luckyroy <107039265+luckyroy5615@users.noreply.github.com>
2022-10-02 11:21:58 +0530

    Update rope.js

--- rope.js
+++ rope.js
@@ -1,27 +1,27 @@
-class rope{
-	constructor(body1,body2, offsetX)
-	{
-		var options={
-			bodyA:body1,
-			bodyB:body2
-		}
+class rope {
+  constructor(body1, body2, offsetX)
+  {
+    var options = {
+      bodyA: body1,
+      bodyB: body2,
+    }
 
-		this.rope=Constraint.create(options)
-		World.add(world,this.rope)
+    this.rope = Constraint.create(options)
+    World.add(world, this.rope)
 
-		this.offsetX = offsetX
-	}
+    this.offsetX = offsetX
+  }
 
-	display()
-	{
-		var pointA=this.rope.bodyA.position;
-		var pointB=this.rope.bodyB.position;
+  display()
+  {
+    var pointA = this.rope.bodyA.position;
+    var pointB = this.rope.bodyB.position;
 
-		strokeWeight(2);
+    strokeWeight(2);
 
-		//WRITE THE CORRECT CODE TO RENDER A LINE BETWEEN THE TWO BODIES
+    //WRITE THE CORRECT CODE TO RENDER A LINE BETWEEN THE TWO BODIES
+    line(pointA.x, pointA.y, pointB.x + this.offsetX, pointB.y);
 
-		
-	}
+  }
 
 }
